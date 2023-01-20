939ee3ad1f798a79928c901b4a0b12cf307af1f7
askmadden17 <maddenm3@yahoo.com>
2023-01-20 14:32:31 +0800

    success

--- pages/success.js
+++ pages/success.js
@@ -7,7 +7,7 @@ import { Inter } from '@next/font/google'
 const inter = Inter({ subsets: ['latin'] })
 
 
-export default function Services(){
+export default function Success(){
     return(
         <>
             <Head>
